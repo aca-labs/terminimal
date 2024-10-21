99a99cc3881dbd1cf15fafc7f992fadb88d70bd8
Viv B <viv@place.technology>
2024-10-22 10:21:33 +1100

    fix(future): Future::Compute

--- src/terminimal.cr
+++ src/terminimal.cr
@@ -35,7 +35,7 @@ module Terminimal
   end
 
   # ditto
-  def spinner(await : Future, style = Spinner::Style::UNI_DOTS, async = false, &message : Proc(String))
+  def spinner(await : Future::Compute, style = Spinner::Style::UNI_DOTS, async = false, &message : Proc(String))
     future_completed = ->{ await.completed? || await.canceled? }
     spinner(future_completed, style, async, &message)
   end
@@ -46,7 +46,7 @@ module Terminimal
   end
 
   # ditto
-  def spinner(await : Future, style = Spinner::Style::UNI_DOTS, async = false, message = "")
+  def spinner(await : Future::Compute, style = Spinner::Style::UNI_DOTS, async = false, message = "")
     spinner(await, style, async) { message }
   end
 
